9aea60101c8dab2a46bef7f398e98b0076cc5ce0
Owain Lewis <owain.lewis@oracle.com>
2023-12-19 16:44:16 +0000

    Tidy up Main.java

--- src/main/java/com/dispatch/server/Main.java
+++ src/main/java/com/dispatch/server/Main.java
@@ -4,15 +4,14 @@ import com.dispatch.server.config.Config;
 import com.dispatch.server.config.ConfigReader;
 
 public class Main {
+    private static final String CONFIG = "src/main/resources/config.yml";
 
     public static void main(String[] args) throws Exception {
-
+        // Load configuration
         ConfigReader reader = new ConfigReader();
+        Config config = reader.read(CONFIG);
 
-        Config config = reader.read("src/main/resources/config.yml");
-
-        System.out.println(config);
-
+        // Start server with defaults
         new DispatchServer().start(8080);
     }
 }
